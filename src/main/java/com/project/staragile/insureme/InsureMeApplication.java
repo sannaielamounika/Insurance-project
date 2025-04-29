3f9ff9aae309833afd6f9d076d65863471fa8f7c
mounika <sannaielamounika12gmail.com>
2025-04-29 20:06:23 +0000

    modified InsureMeApplication file

--- src/main/java/com/project/staragile/insureme/InsureMeApplication.java
+++ src/main/java/com/project/staragile/insureme/InsureMeApplication.java
@@ -1,24 +1,13 @@
 package com.project.staragile.insureme;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InsureMeApplication implements CommandLineRunner {
+public class InsureMeApplication {
 
-    @Autowired
-    private PolicyRepository policyRepository;
+	public static void main(String[] args) {
+		SpringApplication.run(InsureMeApplication.class, args);
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(InsureMeApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        policyRepository.save(new Policy(0, "Alice", "Health", 12000, "01-Jan-2023", "31-Dec-2023"));
-        policyRepository.save(new Policy(0, "Bob", "Life", 15000, "01-Feb-2023", "31-Jan-2024"));
-        policyRepository.save(new Policy(0, "Charlie", "Car", 8000, "15-Mar-2023", "14-Mar-2024"));
-    }
 }
